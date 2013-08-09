0923348bd660b54b8ffbab1de81bf315827d1392
mpm <mpmiszczyk@gmail.com>
2013-08-09 16:09:14 +0200

    * test/sk_utils_test.erl: Refactoring: introduced steup/teardown functionality to remove code duplication

--- test/sk_utils_test.erl
+++ test/sk_utils_test.erl
@@ -5,10 +5,19 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
-start_workers_shoul_create_N_workers_test() ->
+sk_utils_test_() ->
+  {foreach, fun setup/0, fun teardown/1,
+   [ fun start_workers_shoul_create_N_workers/0,
+     fun stop_workers_should_stop_all_workers/0]}.
 
+setup() ->
+  meck:new(sk_utils,[passthrough]).
+
+teardown( _ ) ->
+  meck:unload(sk_utils).
+
+start_workers_shoul_create_N_workers() ->
   %% given
-  meck:new(sk_utils,[passthrough]),
   meck:expect(sk_utils, start_worker, 2, pid),
   NumOfWorkers = 10,
 
@@ -20,15 +29,11 @@ start_workers_shoul_create_N_workers_test() ->
                 length( Workers )),
   ?assert( meck:validate( sk_utils )),
   ?assertEqual( NumOfWorkers,
-                meck:num_calls( sk_utils, start_worker,'_')),
-
-  meck:unload(sk_utils).
+                meck:num_calls( sk_utils, start_worker,'_')).
 
 
-stop_workers_should_stop_all_workers_test() ->
-
+stop_workers_should_stop_all_workers() ->
   %% given
-  meck:new(sk_utils,[passthrough]),
   meck:expect(sk_utils, stop_worker, 1, {system, eos}),
   NumOfWorkers = 10,
   Workers = lists:seq(1, NumOfWorkers),
@@ -43,7 +48,5 @@ stop_workers_should_stop_all_workers_test() ->
   ?assertEqual( 1,
                 meck:num_calls( sk_utils, stop_worker, [1])),
   ?assertEqual( 1,
-                meck:num_calls( sk_utils, stop_worker, [NumOfWorkers])),
-
-  meck:unload(sk_utils).
+                meck:num_calls( sk_utils, stop_worker, [NumOfWorkers])).
 
